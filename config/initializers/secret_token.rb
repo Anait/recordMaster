d8a1e394ff188cd5c27c002f89d29930d167fe0a
Anait Markosian <ana_markosian@yahoo.com>
2012-11-10 21:53:24 -0800

    Generate the Rails 3 application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RecordMaster::Application.config.secret_token = '302c21385a2ada764b74fc48e94e4daecb4509e5ac571f41c1e8a6f2c715ba667938f4bbba3bea1a4aeeb14d77a00076a478c8df3bdf57fc81287d00c85e7236'
